ddc41b9cf45e9b0aa3f093060398076d581044a7
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-09-21 18:12:33 -0300

    build: re-creating account and final trading

--- app/views/trade_view.py
+++ app/views/trade_view.py
@@ -1,11 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from app.models import User
+from decimal import Decimal
 
 @login_required(login_url='login')
 def trading(request):
+    if request.method == "POST":
+        user = get_object_or_404(User, pk=request.user.id)
+        value_deposited= request.POST.get('depositedValue')
+        user.amount += Decimal(value_deposited)
+        user.save()
+
+        text: str = f"Valor depositado com sucesso! - {value_deposited}"
+        messages.success(request, text)
+        return redirect('trading')
+
     context = {
         "title_site": 'Urubu do pix - trade',
-        'message': True
     }
 
     return render(request, 'pages/trading.html', context)
