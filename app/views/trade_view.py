fd08d447bafd22c64f1501e1791cfdfe7ac51e42
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-09-21 19:30:25 -0300

    refactor: refact codde and creating validations

--- app/views/trade_view.py
+++ app/views/trade_view.py
@@ -2,15 +2,20 @@ from django.shortcuts import render,redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from app.models import User
-from decimal import Decimal
+from decimal import Decimal, InvalidOperation  
 
 @login_required(login_url='login')
 def trading(request):
     if request.method == "POST":
         user = get_object_or_404(User, pk=request.user.id)
         value_deposited = request.POST.get('depositedValue')
-        if Decimal(value_deposited) <= 0:
-            text: str = f"Valor depositado invalido tente um valor positivo! - R${value_deposited}"
+        try:
+            if Decimal(value_deposited) <= 0:
+                text: str = f"Valor depositado invalido tente um valor positivo! - R${value_deposited}"
+                messages.error(request, text)
+                return redirect('trading')
+        except InvalidOperation:
+            text: str = f"O valor não pode ser um texto! - Tente novamente"
             messages.error(request, text)
             return redirect('trading')
 
