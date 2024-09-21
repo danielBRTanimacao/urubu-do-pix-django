5fca916af6f468ec51862a533df3dd0f1c065591
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-09-21 10:54:07 -0300

    build: features to create and login created logicals and errors

--- app/forms.py
+++ app/forms.py
@@ -1,18 +1,59 @@
 from .models import User
 from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
-class UserForm(forms.ModelForm):
+class RegisterForm(UserCreationForm):
 
     username = forms.CharField(
         label='Nome de usuario',
-        widget=forms.TextInput(
+        widget=forms.TextInput(),
+        required=True,
+        min_length=3,
+    )
+    password1 = forms.CharField(
+        label='Senha',
+        widget=forms.PasswordInput(
             attrs={
-                'class': 'input'
+                'id': 'passwordInput',
             }
         ),
-        required=True
+        required=True,
+        min_length=3
+    )
+    password2 = forms.CharField(
+        label='Confirmar senha',
+        widget=forms.PasswordInput(),
+        required=True,
+        min_length=3
     )
 
     class Meta:
         model = User
-        fields = ['username', 'password1', 'password2']
+        fields = (
+            'username', 'password1', 'password2',
+        )
+    
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(
+        label='Seu nome',
+        widget=forms.TextInput(),
+        required=True,
+        min_length=3,
+    )
+    password = forms.CharField(
+        label='Senha',
+        widget=forms.PasswordInput(
+            attrs={
+                'id': 'passwordInput'
+            }
+        ),
+        required=True,
+        min_length=3,
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            'username', 'password'
+        )
--- app/views/form_view.py
+++ app/views/form_view.py
@@ -1,21 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-
+from django.contrib.auth import login, logout
+from app.forms import RegisterForm, LoginForm
 
 def create(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+    
+    form = RegisterForm()
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+
     context = {
-        'site_title': 'Registrar - Urubu do pix'
+        'site_title': 'Registrar - Urubu do pix',
+        'form': form
     }
 
-    return render(request, 'pages/index.html', context)
+    return render(request, 'pages/create.html', context)
     
 def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+    
+    form = LoginForm()
+    if request.method == "POST":
+        form = LoginForm(request, data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('account')
+        
     context = {
-        'site_title': 'Login - Urubu do pix'
+        'site_title': 'Login - Urubu do pix',
+        'form': form 
     }
 
-    return render(request, 'pages/index.html', context)
+    return render(request, 'pages/login.html', context)
 
 @login_required(login_url='login')
 def logout(request):    
-    return render(request, 'pages/index.html')
+    logout(request)
+    return redirect('login')
